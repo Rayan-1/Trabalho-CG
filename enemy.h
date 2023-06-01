96f419087fc4716b5d7015acbb9521156e9c3786
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:46:40 -0300

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,19 +1,31 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#include <GL/gl.h>
+
+class Player; // Declaração antecipada da classe Player
+
 class Enemy {
 public:
-    Enemy();
+    Enemy(Player& player); // Construtor que recebe uma referência para o objeto Player
 
-    void draw() const;
     void update();
+    void draw() const;
+    void checkCollision();
+
+    float getPlayerPositionX() const;
+    float getPlayerPositionY() const;
+    float getPlayerWidth() const;
+    float getPlayerHeight() const;
 
 private:
+    Player& player;
     float positionX;
     float positionY;
     float speed;
     float width;
     float height;
+
 };
 
 #endif
