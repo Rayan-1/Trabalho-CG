fcf3816ab05ecc7afb7fef10c4b2d525847b98cc
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:47:20 -0300

    Update input.h

--- input.h
+++ input.h
@@ -4,6 +4,10 @@
 class Input {
 public:
     static bool KeyIsPressed(int key);
+    static void KeyboardKeyDown(unsigned char key, int x, int y);
+    static void KeyboardKeyUp(unsigned char key, int x, int y);
+    static void SpecialKeyDown(int key, int x, int y);
+    static void SpecialKeyUp(int key, int x, int y);
 };
 
 #endif
