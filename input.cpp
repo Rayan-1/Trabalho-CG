78fcd2e6f3402d2c35e09a80ba4e7d2d141b08f1
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:47:01 -0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,7 +1,28 @@
 #include "input.h"
-#include <GLFW/glfw3.h>
 
-bool Input::KeyIsPressed(int key) {
-    int state = glfwGetKey(glfwGetCurrentContext(), key);
-    return state == GLFW_PRESS || state == GLFW_REPEAT;
+bool Input::keyStates[256] = { false };
+bool Input::specialKeyStates[256] = { false };
+
+void Input::KeyboardKeyDown(unsigned char key, int x, int y) {
+    keyStates[key] = true;
+}
+
+void Input::KeyboardKeyUp(unsigned char key, int x, int y) {
+    keyStates[key] = false;
+}
+
+void Input::SpecialKeyDown(int key, int x, int y) {
+    specialKeyStates[key] = true;
+}
+
+void Input::SpecialKeyUp(int key, int x, int y) {
+    specialKeyStates[key] = false;
+}
+
+bool Input::KeyIsPressed(unsigned char key) {
+    return keyStates[key];
+}
+
+bool Input::SpecialKeyIsPressed(int key) {
+    return specialKeyStates[key];
 }
