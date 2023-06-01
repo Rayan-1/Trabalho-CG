6ff4a02a9dd0c6ed9e6e97c9a00008b240cd80a0
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:45:30 -0300

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -1,7 +1,11 @@
 #include "enemy.h"
-#include <GL/gl.h>
+#include "player.h"
+#include <cstdlib>
+#include <GL/glut.h>
 
-Enemy::Enemy() {
+Enemy::Enemy(Player& player) 
+      : player(player)
+{
     positionX = 200;
     positionY = 200;
     speed = 2;
@@ -15,15 +19,35 @@ void Enemy::update() {
     positionY += speed;
 
     // Lógica de movimento ou comportamento do inimigo
-    // ...
+    // Exemplo de movimento aleatório:
+    if (rand() % 100 < 10) { // 5% de chance de mudar de direção a cada frame
+        speed = -speed; // Inverte a direção do movimento
+    }
+    checkCollision();
+   
 
-    // Verifica colisões ou outras interações com o jogador ou obstáculos
     // ...
+}
+
+void Enemy::checkCollision() {
+    float playerX = player.getPositionX();
+    float playerY = player.getPositionY();
+    float playerWidth = player.getWidth();
+    float playerHeight = player.getHeight();
+
+    if (positionX < playerX + playerWidth &&
+        positionX + width > playerX &&
+        positionY < playerY + playerHeight &&
+        positionY + height > playerY) {
+        // Colisão com o jogador, execute a ação desejada
+        // Exemplo: diminua a vida do jogador ou destrua o inimigo
+    }
 }
 
 void Enemy::draw() const {
     glPushMatrix();
     glTranslatef(positionX, positionY, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f); // Define a cor vermelha
     glBegin(GL_QUADS);
     glVertex2f(0.0f, 0.0f);
     glVertex2f(width, 0.0f);
@@ -31,4 +55,22 @@ void Enemy::draw() const {
     glVertex2f(0.0f, height);
     glEnd();
     glPopMatrix();
+
+}
+
+// Implementação das funções auxiliares para obter informações do jogador
+float Enemy::getPlayerPositionX() const {
+    return player.getX();
+}
+
+float Enemy::getPlayerPositionY() const {
+    return player.getY();
+}
+
+float Enemy::getPlayerWidth() const {
+    return player.getWidth();
+}
+
+float Enemy::getPlayerHeight() const {
+    return player.getHeight();
 }
