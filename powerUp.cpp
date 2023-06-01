888828b457ba0cece3ceff7efd9dd1d23ee0b1ef
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:49:50 -0300

    Update powerUp.cpp

--- powerUp.cpp
+++ powerUp.cpp
@@ -1,13 +1,18 @@
 #include "powerUp.h"
+#include "player.h"
 #include <GL/gl.h>
 #include <cmath>
 
-PowerUp::PowerUp() {
+PowerUp::PowerUp(const Player& player) 
+    : player(player)
+ {
     positionX = 100;
     positionY = 100;
     speed = 1;
     radius = 20;
     isActive = true;
+    width = 0; // Inicialize a variável width conforme necessário
+    height = 0; // Inicialize a variável height conforme necessário
 }
 
 void PowerUp::update() {
@@ -19,7 +24,18 @@ void PowerUp::update() {
     // ...
 
     // Verifica colisões ou outras interações com o jogador ou obstáculos
-    // ...
+    float playerX = player.getPositionX();
+    float playerY = player.getPositionY();
+    float playerWidth = player.getWidth();
+    float playerHeight = player.getHeight();
+
+    if (positionX < playerX + playerWidth &&
+        positionX + width > playerX &&
+        positionY < playerY + playerHeight &&
+        positionY + height > playerY) {
+        // Colisão com o jogador, execute a ação desejada
+        // Exemplo: adicione pontos ao jogador ou ative um efeito especial
+    }
 }
 
 void PowerUp::draw() const {
@@ -32,7 +48,7 @@ void PowerUp::draw() const {
     // Desenha o Power-Up como um círculo
     const int numSegments = 30;
     glBegin(GL_TRIANGLE_FAN);
-    glColor3f(1.0f, 1.0f, 0.0f); // Cor amarela
+    glColor3f(0.0f, 0.0f, 0.0f); // Define a cor preta para a bolinha
     glVertex2f(0.0f, 0.0f); // Posição central do círculo
     for (int i = 0; i <= numSegments; i++) {
         float theta = 2.0f * 3.1415926f * float(i) / float(numSegments);
