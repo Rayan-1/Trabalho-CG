a78d5535613b7ddaf35a9b5b1bb2df4267e259eb
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-01 02:49:27 -0300

    Update player.h

--- player.h
+++ player.h
@@ -2,6 +2,9 @@
 #define PLAYER_H
 
 #include <GL/gl.h>
+#include <iostream>
+#include "enemy.h"
+
 
 class Player {
 public:
@@ -12,6 +15,16 @@ public:
     void moveRight();
     void moveUp();
     void moveDown();
+    bool collidesWith(const Enemy& enemy) const;
+
+    
+    float getX() const { return positionX; }
+    float getY() const { return positionY; }
+    float getWidth() const { return width; }
+    float getHeight() const { return height; }
+
+    float getPositionX() const;
+    float getPositionY() const;
 
 private:
     float positionX;
